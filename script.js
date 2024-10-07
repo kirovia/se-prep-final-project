1001fbc9c274175015ca4b9dfddd33a0b5cd8aad
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2024-10-06 23:56:00 -0400

    add content to index.html, style added content in style.css, and store some elements as variables in script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const topLeft = document.getElementById(`top-left`);
+const topRight = document.getElementById(`top-right`);
+const bottomLeft = document.getElementById(`bottom-left`);
+const bottomRight = document.getElementById(`bottom-right`);
+
+let topLeftToggle = 0;
+let topRightToggle = 0;
+let bottomLeftToggle = 0;
+let bottomRightToggle = 0;
+
